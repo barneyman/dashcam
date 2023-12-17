71d7decf0935150bba56462561e1fef2c9e8fc29
barney <barnaby.james.flint@gmail.com>
2023-12-17 07:49:11 +0000

    revert to image save

--- stage-docker-chrony/00-install-docker/00-run.sh
+++ stage-docker-chrony/00-install-docker/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 mkdir -p ${ROOTFS_DIR}/var/firstboot
-#docker pull ghcr.io/barneyman/gpsd-chrony-ntp
-#docker save ghcr.io/barneyman/gpsd-chrony-ntp | gzip > ${ROOTFS_DIR}/var/firstboot/1.tar.gz
+docker pull ghcr.io/barneyman/gpsd-chrony-ntp
+docker save ghcr.io/barneyman/gpsd-chrony-ntp | gzip > ${ROOTFS_DIR}/var/firstboot/1.tar.gz
 
 #cp files/docker-compose.yml ${ROOTFS_DIR}/var/firstboot/
 cp files/docker-compose-app.service ${ROOTFS_DIR}/etc/systemd/system/
@@ -28,37 +28,37 @@ on_chroot << EOF
 EOF
 
 
+### Doesn't work in actions
+# ## point docker to my partition and pull
 
-## point docker to my partition and pull
+# # preserve any existing config
+# if [ -e /etc/docker/daemon.json ]
+# then
+# 	mv /etc/docker/daemon.json  /etc/docker/daemon.json.old
+# fi
 
-# preserve any existing config
-if [ -e /etc/docker/daemon.json ]
-then
-	mv /etc/docker/daemon.json  /etc/docker/daemon.json.old
-fi
+# # point to my 'future' var/lib/docker
+# cat > /etc/docker/daemon.json << EOF
+# {
+#   "data-root": "${ROOTFS_DIR}/var/lib/docker"
+# }
+# EOF
 
-# point to my 'future' var/lib/docker
-cat > /etc/docker/daemon.json << EOF
-{
-  "data-root": "${ROOTFS_DIR}/var/lib/docker"
-}
-EOF
+# # restart docker
+# systemctl restart docker
 
-# restart docker
-systemctl restart docker
+# # pull image(s)
+# docker pull ghcr.io/barneyman/gpsd-chrony-ntp
 
-# pull image(s)
-docker pull ghcr.io/barneyman/gpsd-chrony-ntp
-
-# kill my hacked config
-rm /etc/docker/daemon.json
-# and reinstate the old config
-if [ -e /etc/docker/daemon.json.old ]
-then
-        mv /etc/docker/daemon.json.old  /etc/docker/daemon.json
-fi
-# and start the host docker again
-systemctl restart docker
+# # kill my hacked config
+# rm /etc/docker/daemon.json
+# # and reinstate the old config
+# if [ -e /etc/docker/daemon.json.old ]
+# then
+#         mv /etc/docker/daemon.json.old  /etc/docker/daemon.json
+# fi
+# # and start the host docker again
+# systemctl restart docker
 
 
 # rpihaddev 3B - armv7l
