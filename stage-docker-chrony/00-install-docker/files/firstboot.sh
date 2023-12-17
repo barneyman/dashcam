435f93dce2e5ef890636efef04d66e5da57d4780
barney <barnaby.james.flint@gmail.com>
2023-12-17 06:05:41 +0000

    try docker pull direct

---
+++ stage-docker-chrony/00-install-docker/files/firstboot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Starting firstboot.sh ..." 2>&1
+# leave a tell tale
+touch firstbootdone
+echo "Left breadcrumb behind" 2>&1
+# load all the images
+#for f in ./*.gz; do echo Loading $f && echo "Loading $f docker image ..." 2>&1 && docker load < $f && rm $f; done
+echo "Bringing docker up ..." 2>&1
+# start docker
+#/usr/bin/docker compose up -d
