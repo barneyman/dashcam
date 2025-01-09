3f770aa98fe47ba9e0918de4f8b2ac0c9a9bd3e6
barneyman <barnaby.james.flint@gmail.com>
2025-01-09 22:25:16 +1100

    pwd changes

--- buildx/other/mysql/scripts/aab-create-users.sql
+++ buildx/other/mysql/scripts/aab-create-users.sql
@@ -1,5 +1,5 @@
 use mysql;
 CREATE USER 'dashcam'@'%' IDENTIFIED BY 'dashcam';
-ALTER USER 'dashcam'@'%' IDENTIFIED WITH mysql_native_password BY 'dashcam';
+ALTER USER 'dashcam'@'%' IDENTIFIED WITH caching_sha2_password BY 'dashcam';
 GRANT EXECUTE ON dashcam.* TO 'dashcam'@'%';
 FLUSH PRIVILEGES;
