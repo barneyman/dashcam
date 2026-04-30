b6e154e3d315095d0ba9756247e92f45e11e2c05
barneyman <barnaby.james.flint@gmail.com>
2026-04-30 16:24:17 +1000

    debug

--- stage-docker-chrony/00-seed-chrony/00-run.sh
+++ stage-docker-chrony/00-seed-chrony/00-run.sh
@@ -3,6 +3,8 @@
 cp files/chrony.tar.gz ${ROOTFS_DIR}/tmp/
 cp files/docker-compose.yml ${ROOTFS_DIR}/var/firstboot/
 
+tree ${ROOTFS_DIR}/tmp/
+
 on_chroot << EOF
 
     docker load -i /tmp/chrony.tar.gz
